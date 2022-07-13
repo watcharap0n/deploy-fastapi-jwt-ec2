e2ef8790690a6115c2db486c609ec03777bebf4b
watcharapon weeraborirak <wera.watcharapon@gmail.com>
2022-07-13 10:22:46 +0700

    dashboard user

---
+++ dashboard_user/app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.api import user
+
+app = FastAPI(
+    version='1.0.0',
+    openapi_url='/dashboard/openapi.json',
+    docs_url='/dashboard/docs',
+    redoc_url='/dashboard/redoc'
+)
+
+app.include_router(
+    user.router,
+    prefix='/user',
+    tags=['User'],
+    responses={418: {"description": "I'm teapot"}},
+)
