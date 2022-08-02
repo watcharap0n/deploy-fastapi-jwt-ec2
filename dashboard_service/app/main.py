87bf19d0158a42c29a41a64a9239ac875635b085
watcharapon weeraborirak <69833839+watcharap0n@users.noreply.github.com>
2022-08-02 16:10:22 +0700

    cors origin resource

--- dashboard_service/app/main.py
+++ dashboard_service/app/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
 from app.api import user
 from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     version='1.0.0',
     openapi_url='/api/v1/dashboard/openapi.json',
     docs_url='/api/v1/dashboard/docs',
     redoc_url='/api/v1/dashboard/redoc'
+)
+
+origins = [
+    "http://localhost:80",
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(
